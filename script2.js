d7627985c26bad572ed9a9017c397fa38c6be28f
Pavel Yakovlev <pavel.yakovlev.1984@bk.ru>
2020-10-13 17:41:43 +0300

    hard_lesson02

---
+++ script2.js
@@ -0,0 +1,17 @@
+let num = 266219;
+let number = String(num);
+let sum = 1;
+// let num2 = number.split(/(\d)/);
+// console.log(num2);
+
+
+// var myString = 'Привет 1 мир. Предложение номер 2.';
+// var splits = myString.split(/(\d)/);
+
+// console.log(splits);
+for(let i=0; i<6; i++){
+
+ sum = sum* number[i]
+  
+  console.log(sum);
+}
