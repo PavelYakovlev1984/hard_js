2d07af634bf908806d1aea4ff9e87265baa39c19
Pavel Yakovlev <pavel.yakovlev.1984@bk.ru>
2020-10-13 19:17:19 +0300

    стер коментарии

--- script2.js
+++ script2.js
@@ -11,12 +11,10 @@ for(let i = 0; i < number.length; i++){
   console.log(sum);
 }
 
-// let degree = 0;
-// let b = 3;
-// degree = (sum ** b);
-let squared = 3 ** 2; // 9
+
+
  
-//  num = 3;
+
 num **= 2;
 console.log(num);
 
